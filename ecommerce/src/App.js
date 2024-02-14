52668ff4ee46ef21444713c905b0d3d1fb8b8dec
raut amol <84304332+amolraut474@users.noreply.github.com>
2024-02-14 21:53:35 +0530

    Update App.js

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header_Com/Header";
 import Footer from "./components/Footer_Com/Footer";
 import ProductDynamic from "./components/Product_Com/ProductDynamic";
@@ -10,30 +10,27 @@ import Pages from "./pages/Pages";
 import Products from "./pages/Products";
 import Blog from "./pages/Blog";
 import Shop from "./pages/Shop";
-import Contact from "./pages/Contact Us";
+import Contact from "./pages/Contact";
 
-
-// Components
 function App() {
   return (
-  <div className="App">
- <header><Header/></header>
- <Routes>
-  <Route path='/' element={<Home/>}></Route>
-  <Route path='/Pages' element={<Pages/>}></Route>
-  <Route path='/Products' element={<Products/>}></Route>
-  <Route path='/Shop' element={<Shop/>}></Route>
-  <Route path='/Blog' element={<Blog/>}></Route>
-  <Route path='/Contact us' element={<Contact us/>}></Route>
-  <Route path="/" exact component={<ProductList/>}/>
-  <Route path="/Products/:productId" element={<ProductDynamic/>}/>
-</Routes>
-<footer><Footer/></footer>
-</div>
-
+    <Router>
+      <div className="App">
+        <header><Header/></header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Pages" element={<Pages />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/Products/:productId" element={<ProductDynamic />} />
+        </Routes>
+        <footer><Footer/></footer>
+      </div>
+    </Router>
   );
- 
 }
 
 export default App;
-
